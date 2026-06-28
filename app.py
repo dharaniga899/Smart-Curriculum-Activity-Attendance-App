456a6262a18102636d8b6f3bb37dafaec992514c
dharaniga899 <dharanigaraj@gmail.com>
2026-06-28 23:54:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-students = [
-    {"id": 1, "name": "Arun", "attendance": "Present"},
-    {"id": 2, "name": "Priya", "attendance": "Absent"},
-    {"id": 3, "name": "Rahul", "attendance": "Present"}
-]
-
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/dashboard')
 def dashboard():
+    students = [
+        {"id": 1, "name": "Dharani", "attendance": "95%"},
+        {"id": 2, "name": "Priya", "attendance": "90%"}
+    ]
     return render_template('dashboard.html', students=students)
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
